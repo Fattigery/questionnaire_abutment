cd762b0673f77b14b61953d424af7dc533292905
张强 <51077671+Fattigery@users.noreply.github.com>
2024-02-23 13:37:32 +0800

    更新

--- I67066.js
+++ I67066.js
@@ -4,9 +4,9 @@ let sid = uuid.substring(0, uuid.indexOf('='));
 let nid = uuid.substring(uuid.indexOf('=') + 1);
 
 let arid = {
-	1: ``,
-	2: ``,
-	3: ``
+	1: `http://medpaneljunction.surveybackoffice.com/endcapture.php?cada=MTEwODgtYldWa2NHRnVaV3hxZFc1amRHbHZiZz09&st=111`,
+	2: `http://medpaneljunction.surveybackoffice.com/endcapture.php?cada=MTEwODgtYldWa2NHRnVaV3hxZFc1amRHbHZiZz09&st=222`,
+	3: `http://medpaneljunction.surveybackoffice.com/endcapture.php?cada=MTEwODgtYldWa2NHRnVaV3hxZFc1amRHbHZiZz09&st=333`
 };
 
 // 如果有标识才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
