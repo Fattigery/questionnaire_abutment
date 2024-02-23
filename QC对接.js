0cfc4107978263a6877cdbffb9b6ec42544fbe8a
张强 <51077671+Fattigery@users.noreply.github.com>
2024-02-23 09:02:17 +0800

    更新

---
+++ QC对接.js
@@ -0,0 +1,65 @@
+// currentStateNumber：状态记录
+// project：国家标识
+// scid：供应商标识
+// uid：用户id
+
+let US = {
+	arid: {
+		1: ``,
+		2: ``,
+		3: ``
+	},
+	brid: {
+		1: ``,
+		2: ``,
+		3: ``
+	}
+};
+
+let UK = {};
+
+// 印度（英语）
+let IN = {};
+
+// 韩国
+let KR = {};
+
+// 日本
+let JP = {};
+
+// 中国
+let CN = {};
+
+// 西班牙
+let SP = {};
+
+// 法国
+let FR = {};
+
+// 印尼 INDO
+let ID = {};
+
+// 巴西 BR
+let BR = {};
+
+// 德国
+let DE = {};
+
+// 获取对应的链接并跳转
+if (scid) {
+	hiddenEl();
+	skip(eval(project)?.[scid]?.[currentStateNumber]);
+}
+
+// 跳转函数
+function skip(url) {
+	if (url == '') return;
+	location.href = url;
+}
+
+function hiddenEl() {
+	let textEL = document.querySelector('#text');
+	if (textEL) {
+		textEL.style.display = 'none';
+	}
+}
