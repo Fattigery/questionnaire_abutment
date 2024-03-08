d70c626145cd3926b912da5b21ef0cff2052d9ea
张强 <51077671+Fattigery@users.noreply.github.com>
2024-03-08 11:19:16 +0800

    更新

--- linkskip.js
+++ linkskip.js
@@ -12,5 +12,16 @@ let 项目名 = {
 };
 
 let AAA = {
-	a: `https://www.baidu.com/s?wd=你好啊${uid}`
+	a: `https://www.baidu.com/s?wd=李银河${uid}`
 };
+
+// 如果有标识才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
+if (project) {
+	skip(eval(project)?.[mark]);
+}
+
+function skip(url) {
+	console.log(url);
+	if (url == '') return;
+	location.href = url;
+}
