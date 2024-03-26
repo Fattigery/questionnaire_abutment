24ed15e3bcc4ba71603eeb0bcab39c5a0065a0ab
张强 <51077671+Fattigery@users.noreply.github.com>
2024-03-26 11:05:33 +0800

    更新

---
+++ S81517.js
@@ -0,0 +1,29 @@
+// uuid：arid=VALUE
+// 获取标识
+let sid = uuid.substring(0, uuid.indexOf('='));
+// 获取id
+let nid = uuid.substring(uuid.indexOf('=') + 1);
+
+let app = {
+	1: `https://3asresearchsolutions.in/PMT/public/admin/c2s?pcode=${nid}&pnum=AC30-A5343&status=1`,
+	2: `https://3asresearchsolutions.in/PMT/public/admin/c2s?pcode=${nid}&pnum=AC30-A5343&status=2`,
+	3: `https://3asresearchsolutions.in/PMT/public/admin/c2s?pcode=${nid}&pnum=AC30-A5343&status=3`
+};
+
+// 如果有标识并且有对应的变量才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
+try {
+	// 使用eval()函数来检查变量是否存在。
+	eval(sid);
+	// 如果变量存在，则会执行到这里。
+	console.log('变量存在');
+	skip(eval(sid)?.[stateNumber]);
+} catch (error) {
+	// 如果变量不存在，则会捕获到异常。
+	console.log('变量不存在或链接获取失败');
+}
+
+function skip(url) {
+	console.log(url);
+	if (!url) return;
+	location.href = url;
+}
--- linkskip.js
+++ linkskip.js
@@ -6,23 +6,23 @@
  * uid：ID
  */
 
-let 项目名 = {
-	标识1: '链接',
-	标识2: '链接'
-};
-
-// 测试
-let AAA = {
-	a: `https://www.baidu.com/s?wd=李银河----${uid}`
+let S81517 = {
+	a: `https://s1.intellisurvey.com/pub/is029027m?pan=218&d1=1&o1=2&co=8&trans_id=app=${uid}`
 };
 
 // 如果有标识才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
-if (project) {
+try {
+	// 检测是否有对应的变量
+	eval(project);
+	// 如果存在，则跳转
 	skip(eval(project)?.[mark]);
+} catch (error) {
+	console.log('不存在对应的project项目对象或者获取对应的链接失败');
 }
 
 function skip(url) {
 	console.log(url);
-	if (url == '') return;
+	// 如果url的值为false（取反为true），则return，不进行跳转
+	if (!url) return;
 	location.href = url;
 }
