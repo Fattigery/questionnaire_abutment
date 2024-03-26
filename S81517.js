4d7d98f64d1b6024c07621482244bb79fc2c79b1
张强 <51077671+Fattigery@users.noreply.github.com>
2024-03-26 13:19:09 +0800

    更新

--- S81517.js
+++ S81517.js
@@ -5,6 +5,24 @@ let sid = uuid.substring(0, uuid.indexOf('x'));
 let nid = uuid.substring(uuid.indexOf('x') + 1);
 
 let app = {
+	1: `https://api.catpanel.cn/business/renwu/design/call/otherSuccessUrl?surveyId=979367921400676352&uid=${nid}`,
+	2: `https://api.catpanel.cn/business/renwu/design/call/otherFailUrl?surveyId=979367921400676352&uid=${nid}`,
+	3: `https://api.catpanel.cn/business/renwu/design/call/otherQuotaFull?surveyId=979367921400676352&uid=${nid}`
+};
+
+let arid = {
+	1: `https://message.opinionest.com/survey/survey-callback.aspx?s=3&TOID=${nid}`,
+	2: `https://message.opinionest.com/survey/survey-callback.aspx?s=5&TOID=${nid}`,
+	3: `https://message.opinionest.com/survey/survey-callback.aspx?s=4&TOID=${nid}`
+};
+
+let brid = {
+	1: `https://www.drsay.cn/bk/s/14272_0aa9e3a792e65e2e?uid=${nid}`,
+	2: `https://www.drsay.cn/bk/s/14272_b3640b004c9ff270?uid=${nid}`,
+	3: `https://www.drsay.cn/bk/s/14272_47f0e4c1f11acc3d?uid=${nid}`
+};
+
+let crid = {
 	1: `https://3asresearchsolutions.in/PMT/public/admin/c2s?pcode=${nid}&pnum=AC30-A5343&status=1`,
 	2: `https://3asresearchsolutions.in/PMT/public/admin/c2s?pcode=${nid}&pnum=AC30-A5343&status=2`,
 	3: `https://3asresearchsolutions.in/PMT/public/admin/c2s?pcode=${nid}&pnum=AC30-A5343&status=3`
