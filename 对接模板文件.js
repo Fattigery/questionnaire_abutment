6740c59c2b2fbe621a8840f6c7652f81c0d82330
张强 <51077671+Fattigery@users.noreply.github.com>
2024-04-25 10:06:15 +0800

    更新

--- 对接模板文件.js
+++ 对接模板文件.js
@@ -18,18 +18,30 @@ let brid = {
 
 // 如果有标识并且有对应的变量才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
 try {
+	// 如果sid为假（为空），则抛出异常。
+	if (!sid) {
+		throw Error('没有标识，sid为空');
+	}
+
 	// 使用eval()函数来检查变量是否存在。
 	eval(sid);
+
 	// 如果变量存在，则会执行到这里。
 	console.log('变量存在');
 	skip(eval(sid)?.[stateNumber]);
 } catch (error) {
-	// 如果变量不存在，则会捕获到异常。
-	console.log('变量不存在，只记录id');
+	// 如果捕获到异常，则会执行到这里。
+	if (error.message === '没有标识，sid为空') {
+		console.log(error.message + '，后台只记录id');
+	} else if (error.message.indexOf('is not defined') !== -1) {
+		let endIndex = error.message.indexOf('is not defined') - 1;
+		console.log(`变量${error.message.substring(0, endIndex)}不存在`);
+	} else {
+		console.log(error.message);
+	}
 }
 
 function skip(url) {
 	console.log(url);
-	if (url == '') return;
 	location.href = url;
 }
