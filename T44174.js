74407ef1e09abcdb4bc935aade78c6c9b692a5c0
张强 <51077671+Fattigery@users.noreply.github.com>
2024-04-25 09:06:32 +0800

    更新

--- T44174.js
+++ T44174.js
@@ -18,18 +18,23 @@ let brid = {
 
 // 如果有标识并且有对应的变量才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
 try {
+	// 如果URL为假（为空），则抛出异常。
+	if (!sid) {
+		throw Error('URL为空');
+	}
 	// 使用eval()函数来检查变量是否存在。
 	eval(sid);
 	// 如果变量存在，则会执行到这里。
 	console.log('变量存在');
 	skip(eval(sid)?.[stateNumber]);
 } catch (error) {
+	console.log(error.message);
 	// 如果变量不存在，则会捕获到异常。
-	console.log('变量不存在，只记录id');
+	// console.log(error.message === "URL为空" ? "URL为空" : "变量不存在，只记录id");
 }
 
 function skip(url) {
 	console.log(url);
-	if (url == '') return;
+	// if (!url) return;
 	location.href = url;
 }
