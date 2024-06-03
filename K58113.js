b5d6a9e25e43379344f8ededfe16f437bd7b41f6
张强 <51077671+Fattigery@users.noreply.github.com>
2024-06-03 10:30:00 +0800

    更新

---
+++ K58113.js
@@ -0,0 +1,48 @@
+// uuid：arid=VALUE
+// 获取标识
+let sid = uuid.substring(0, uuid.indexOf('='));
+// 获取id
+let nid = uuid.substring(uuid.indexOf('=') + 1);
+
+let arid = {
+	1: `https://message.insight.rakuten.com/survey/Spassback.do?pid=c9f0c1a34c2f4f69&uid=${nid}&st=c`,
+	2: `https://message.insight.rakuten.com/survey/Spassback.do?pid=c9f0c1a34c2f4f69&uid=${nid}&st=s`,
+	3: `https://message.insight.rakuten.com/survey/Spassback.do?pid=c9f0c1a34c2f4f69&uid=${nid}&st=q`
+};
+
+let brid = {
+	1: ``,
+	2: ``,
+	3: ``
+};
+
+// 如果有标识并且有对应的变量才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
+try {
+	// 如果sid为假（为空），则抛出异常。
+	if (!sid) {
+		throw Error('没有标识，sid为空');
+	}
+
+	// 使用eval()函数来检查变量是否存在。
+	eval(sid);
+
+	// 如果变量存在，则会执行到这里。
+	console.log('变量存在');
+	skip(eval(sid)?.[stateNumber]);
+} catch (error) {
+	// 如果捕获到异常，则会执行到这里。
+	if (error.message === '没有标识，sid为空') {
+		console.log(error.message + '，后台只记录id');
+	} else if (error.message.indexOf('is not defined') !== -1) {
+		let endIndex = error.message.indexOf('is not defined') - 1;
+		console.log(`变量${error.message.substring(0, endIndex)}不存在`);
+	} else {
+		console.log(error.message);
+	}
+}
+
+function skip(url) {
+	console.log(url);
+	if (!url) return;
+	location.href = url;
+}
