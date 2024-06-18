de727ddf39c0997d66caa37479f695fc1ecbf637
张强 <51077671+Fattigery@users.noreply.github.com>
2024-06-18 11:55:35 +0800

    更新

--- Z88929.js
+++ Z88929.js
@@ -16,6 +16,13 @@ let brid = {
 	3: `https://3asresearchsolutions.in/PMT/public/admin/c2s?pcode=${nid}&pnum=AC30-A6448&status=3`
 };
 
+let crid = {
+	1: `http://medpaneljunction.surveybackoffice.com/endcapture.php?cada=MTEwODgtYldWa2NHRnVaV3hxZFc1amRHbHZiZz09&st=111`,
+	2: `http://medpaneljunction.surveybackoffice.com/endcapture.php?cada=MTEwODgtYldWa2NHRnVaV3hxZFc1amRHbHZiZz09&st=222`,
+	3: `http://medpaneljunction.surveybackoffice.com/endcapture.php?cada=MTEwODgtYldWa2NHRnVaV3hxZFc1amRHbHZiZz09&st=333`
+}
+
+
 // 如果有标识并且有对应的变量才跳转（说明有对应的对接链接），否则不跳转，后台只记录id
 try {
 	if (!sid && !nid) {
